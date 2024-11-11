Fixed attention calculation differences

src/nanotron/models/idefics.py
@@ -24,7 +24,7 @@ from nanotron.random import RandomStates, branch_random_state
 from nanotron.scaling.parametrization import SpectralMupParametrizator, StandardParametrizator
 from nanotron.utils import checkpoint_method
 from nanotron.models.llama import GLUActivation, LlamaModel, pad_to_right, Loss
-
+from transformers.modeling_flash_attention_utils import _flash_attention_forward
 
 
 logger = logging.get_logger(__name__)
@@ -139,9 +139,9 @@ class VisionCoreAttention(nn.Module):
     
 class VisionSelfAttention(nn.Module, AttachableStore):
     def __init__(self, config: Idefics3VisionConfig, parallel_config: Optional[ParallelismArgs],
-    tp_pg: dist.ProcessGroup):
+    tp_pg: dist.ProcessGroup, layer_idx: int):
         super().__init__()
-
+        self.layer_idx = layer_idx
         assert (
             config.num_attention_heads % tp_pg.size() == 0
         ), f"Number of attention heads ({config.num_attention_heads}) must be divisible by TP size ({tp_pg.size()})."
@@ -219,10 +219,15 @@ class VisionSelfAttention(nn.Module, AttachableStore):
             flash_attn_with_kvcache,
         )
 
+        hidden_states = hidden_states
+
         qkv_states = self.qkv_proj(
             hidden_states
-        )  # [seq_length, batch_size, n_local_q_heads * d_qk + 2 * n_local_kv_heads * d_qk]
-        q_length, batch_size, _ = qkv_states.shape
+        )  # [batch_size, seq_length, n_local_q_heads * d_qk + 2 * n_local_kv_heads * d_qk]
+        batch_size, q_length, _ = qkv_states.shape
+
+
+        torch.save(qkv_states, f"img_emb_nano_{self.layer_idx}_qkv.pt")
 
         if self.is_gqa:
             query_states, key_states, value_states = torch.split(
@@ -236,18 +241,18 @@ class VisionSelfAttention(nn.Module, AttachableStore):
             )
 
             query_states = (
-                query_states.transpose(0, 1).contiguous().view(batch_size, q_length, self.n_local_q_heads, self.d_qk)
+                query_states.contiguous().view(batch_size, q_length, self.n_local_q_heads, self.d_qk)
             )
             key_states = (
-                key_states.transpose(0, 1).contiguous().view(batch_size, q_length, self.n_local_kv_heads, self.d_qk)
+                key_states.contiguous().view(batch_size, q_length, self.n_local_kv_heads, self.d_qk)
             )
             value_states = (
-                value_states.transpose(0, 1).contiguous().view(batch_size, q_length, self.n_local_kv_heads, self.d_qk)
+                value_states.contiguous().view(batch_size, q_length, self.n_local_kv_heads, self.d_qk)
             )
         else:
             query_states, key_states, value_states = (
-                qkv_states.view(q_length, batch_size, 3, self.n_local_q_heads, self.d_qk)
-                .permute(2, 1, 0, 3, 4)
+                qkv_states.view(batch_size, q_length, 3, self.n_local_q_heads, self.d_qk)
+                .permute(2, 0, 1, 3, 4)
                 .contiguous()
             )  # [3, batch_size, seq_length, n_local_q_heads, d_qk]
 
@@ -267,16 +272,41 @@ class VisionSelfAttention(nn.Module, AttachableStore):
         key_states = key_states.view(batch_size, kv_length, self.n_local_kv_heads, self.d_qk)
         value_states = value_states.view(batch_size, kv_length, self.n_local_kv_heads, self.d_v)
 
-        attention_output = self.attention(
-            query_states=query_states,
-            key_states=key_states,
-            value_states=value_states,
-        )   
+        torch.save(query_states, f"img_emb_nano_{self.layer_idx}_query_states.pt")
+        torch.save(key_states, f"img_emb_nano_{self.layer_idx}_key_states.pt")
+        torch.save(value_states, f"img_emb_nano_{self.layer_idx}_value_states.pt")
 
-        attention_output = (
-            attention_output.contiguous().view(batch_size, q_length, self.n_local_q_heads * self.d_v).transpose(0, 1)
+        # attention_output = self.attention(
+        #     query_states=query_states,
+        #     key_states=key_states,
+        #     value_states=value_states,
+        # )   
+
+        # attention_output = (
+        #     attention_output.contiguous().view(batch_size, q_length, self.n_local_q_heads * self.d_v).transpose(0, 1)
+        # )
+
+        attn_output = _flash_attention_forward(
+            query_states,
+            key_states,
+            value_states,
+            None,
+            q_length,
+            dropout=0.0,
+            sliding_window=getattr(self, "sliding_window", None),
+            use_top_left_mask=False,
+            is_causal=False,
+            # **kwargs,
         )
-        output = self.o_proj(attention_output)
+
+
+        
+        attn_output = attn_output.contiguous().view(batch_size, q_length, self.d_model)
+
+
+        torch.save(attn_output, f"img_emb_nano_{self.layer_idx}_attn_output.pt")
+
+        output = self.o_proj(attn_output)
 
         return {"hidden_states": output, "sequence_mask": sequence_mask}
     
@@ -331,7 +361,8 @@ class VisionEncoderLayer(nn.Module):
         self,
         config: Idefics3VisionConfig,
         parallel_config: Optional[ParallelismArgs],
-        tp_pg: dist.ProcessGroup
+        tp_pg: dist.ProcessGroup,
+        layer_id: int,
     ):
         super().__init__()
 
@@ -339,6 +370,7 @@ class VisionEncoderLayer(nn.Module):
             config,
             parallel_config=parallel_config,
             tp_pg=tp_pg,
+            layer_idx=layer_id,
         )
 
         self.layer_norm1 = TritonLayerNorm(
@@ -359,6 +391,7 @@ class VisionEncoderLayer(nn.Module):
         )
 
         self.tp_mode = parallel_config.tp_mode if parallel_config is not None else TensorParallelLinearMode.ALL_REDUCE
+        self.layer_id = layer_id
 
 
     def forward(
@@ -367,16 +400,32 @@ class VisionEncoderLayer(nn.Module):
         sequence_mask: Union[torch.Tensor, TensorPointer],
     ) -> Dict[str, Union[torch.Tensor, TensorPointer]]:
         residual = hidden_states
+
+        torch.save(hidden_states, f"img_emb_nano_{self.layer_id}_before_ln1.pt")
+
         hidden_states = self.layer_norm1(hidden_states)
+
+        torch.save(hidden_states, f"img_emb_nano_{self.layer_id}_after_ln1.pt")
+
         output = self.self_attn(hidden_states=hidden_states, sequence_mask=sequence_mask)
 
         hidden_states = output["hidden_states"]
 
+        torch.save(hidden_states, f"img_emb_nano_{self.layer_id}_after_attn.pt")
+
         hidden_states = hidden_states + residual
+
+
+        torch.save(hidden_states, f"img_emb_nano_{self.layer_id}_before_ln2.pt")
 
         residual = hidden_states
         hidden_states = self.layer_norm2(hidden_states)
+
+        torch.save(hidden_states, f"img_emb_nano_{self.layer_id}_after_ln2.pt")
+
         hidden_states = self.mlp(hidden_states=hidden_states)["hidden_states"]
+
+        torch.save(hidden_states, f"img_emb_nano_{self.layer_id}_after_mlp.pt")
 
         hidden_states = hidden_states + residual
 
@@ -425,6 +474,7 @@ class VisionTransformer(nn.Module):
                         "config": config.vision_config,
                         "parallel_config": parallel_config,
                         "tp_pg": parallel_context.tp_pg,
+                        "layer_id": i,
                     },
                     module_input_keys={
                         "hidden_states",
@@ -436,7 +486,7 @@ class VisionTransformer(nn.Module):
                     }
                 )
 
-                for _ in range(config.vision_config.num_hidden_layers)
+                for i in range(config.vision_config.num_hidden_layers)
             ]
         )
 
@@ -476,6 +526,9 @@ class VisionTransformer(nn.Module):
             patch_attention_mask=patch_attention_mask,
         )["embeddings"]
 
+
+        torch.save(hidden_states, "img_emb_nano.pt")
+
         patch_attention_mask = patch_attention_mask.view(batch_size, -1)
 
         hidden_encoder_states = {
@@ -483,8 +536,10 @@ class VisionTransformer(nn.Module):
             "sequence_mask": patch_attention_mask,
         }
 
-        for encoder_layer in self.encoder:
+        for i, encoder_layer in enumerate(self.encoder):
             hidden_encoder_states = encoder_layer(**hidden_encoder_states)
+
+            torch.save(hidden_encoder_states["hidden_states"], f"img_emb_nano_{i}.pt")
 
         hidden_states = hidden_encoder_states["hidden_states"]
         hidden_states = self.post_layernorm(input=hidden_states)
@@ -565,15 +620,10 @@ class Idefics3SimpleMLP(nn.Module):
 
         self.input_size = hidden_size * (config.scale_factor ** 2)
         self.output_size = config.llama_config.hidden_size
-
-        self.proj = TensorParallelColumnLinear(
+        self.proj = nn.Linear(
             self.input_size,
             self.output_size,
-            pg=tp_pg,
-            mode=tp_mode,
-            bias=False,
-            async_communication=tp_linear_async_communication,
-            tp_recompute_allgather=parallel_config.tp_recompute_allgather,
+            bias = False
         )
 
     def forward(self, hidden_states):
@@ -587,13 +637,24 @@ class Idefics3Connector(nn.Module):
         config: Idefics3Config,
         parallel_config: Optional[ParallelismArgs],
         tp_pg: dist.ProcessGroup,
+        p2p
     ):
         super().__init__()
         self.scale_factor = config.scale_factor
-        self.modality_projector = Idefics3SimpleMLP(
-            config=config,
-            parallel_config=parallel_config,
-            tp_pg=tp_pg,
+        self.modality_projector = PipelineBlock(
+            p2p=p2p,
+            module_builder=Idefics3SimpleMLP,
+            module_kwargs={
+                "config": config,
+                "parallel_config": parallel_config,
+                "tp_pg": tp_pg,
+            },
+            module_input_keys={
+                "hidden_states"
+            },
+            module_output_keys={
+                "hidden_states"
+            }
         )
 
     def pixel_shuffle(self, x, scale_factor=2):
@@ -610,7 +671,7 @@ class Idefics3Connector(nn.Module):
     
     def forward(self, hidden_states):
         hidden_states = self.pixel_shuffle(hidden_states, self.scale_factor)
-        hidden_states = self.modality_projector(hidden_states)["hidden_states"]
+        hidden_states = self.modality_projector(hidden_states=hidden_states)["hidden_states"]
         return {"hidden_states": hidden_states}
 
 class Idefics3Model(nn.Module):
@@ -638,20 +699,8 @@ class Idefics3Model(nn.Module):
             parallel_config=parallel_config,
         )
 
-        self.connector = PipelineBlock(
-            p2p=self.llama.p2p,
-            module_builder=Idefics3Connector,
-            module_kwargs={
-                "config": config,
-                "parallel_config": parallel_config,
-                "tp_pg": parallel_context.tp_pg,
-            },
-            module_input_keys={
-                "hidden_states"
-            },
-            module_output_keys={
-                "hidden_states"
-            }
+        self.connector = Idefics3Connector(
+            config, parallel_config, parallel_context.tp_pg, self.llama.p2p
         )
 
         self.image_seq_len = int(
@@ -664,6 +713,9 @@ class Idefics3Model(nn.Module):
             inputs_embeds: Union[torch.Tensor, TensorPointer],
             image_hidden_states: Union[torch.Tensor, TensorPointer],
     ) -> Dict[str, Union[torch.Tensor, TensorPointer]]:
+        print(
+            inputs_embeds.shape, image_hidden_states.shape
+        )
         num_images, _, vision_hidden_size = image_hidden_states.shape
         special_image_token_mask = input_ids == self.image_token_id
         new_inputs_embeds = inputs_embeds.clone()
@@ -704,11 +756,15 @@ class Idefics3Model(nn.Module):
         patches_subgrid = patches_subgrid.unfold(dimension=2, size=patch_size, step=patch_size)
         patch_attention_mask = (patches_subgrid.sum(dim=(-1, -2)) == patch_size * patch_size).bool()
 
+        torch.save(pixel_values, "pv_nano.pt")
+
         # Get sequence from the vision encoder
         image_hidden_states = self.vision_model(
             pixel_values=pixel_values,
             patch_attention_mask=patch_attention_mask,
         )["hidden_states"]
+
+        torch.save(image_hidden_states, "img_hid_nano.pt")
 
         # Modality projection & resampling
         image_hidden_states = self.connector(
@@ -716,6 +772,8 @@ class Idefics3Model(nn.Module):
         )
 
         inputs_embeds = self.llama.token_position_embeddings(input_ids=input_ids, input_mask=input_mask)
+
+        torch.save(inputs_embeds["input_embeds"], "input_embeds_nano.pt")
 
         inputs_embeds = self.inputs_merger(
             input_ids=input_ids,

tools/idefics3/convert_hf_to_nanotron.py
@@ -224,11 +224,18 @@ def copy_weights_from_hf_to_nanotron_vision(
         with torch.no_grad():
             nanotron_model.encoder[i].pp_block.layer_norm1.weight.copy_(hf_model.encoder.layers[i].layer_norm1.weight)
 
+        assert (
+            nanotron_model.encoder[i].pp_block.layer_norm1.bias.shape == hf_model.encoder.layers[i].layer_norm1.bias.shape
+        )
+
+        with torch.no_grad():
+            nanotron_model.encoder[i].pp_block.layer_norm1.bias.copy_(hf_model.encoder.layers[i].layer_norm1.bias)
+
         tmp_qkv_proj = torch.cat(
             [
+                hf_model.encoder.layers[i].self_attn.q_proj.weight,
                 hf_model.encoder.layers[i].self_attn.k_proj.weight,
                 hf_model.encoder.layers[i].self_attn.v_proj.weight,
-                hf_model.encoder.layers[i].self_attn.q_proj.weight,
             ],
             dim=0,
         )
@@ -242,9 +249,9 @@ def copy_weights_from_hf_to_nanotron_vision(
         
         tmp_qkv_proj_bias = torch.cat(
             [
+                hf_model.encoder.layers[i].self_attn.q_proj.bias,
                 hf_model.encoder.layers[i].self_attn.k_proj.bias,
                 hf_model.encoder.layers[i].self_attn.v_proj.bias,
-                hf_model.encoder.layers[i].self_attn.q_proj.bias,
             ],
             dim=0,
         )
@@ -280,6 +287,13 @@ def copy_weights_from_hf_to_nanotron_vision(
 
         with torch.no_grad():
             nanotron_model.encoder[i].pp_block.layer_norm2.weight.copy_(hf_model.encoder.layers[i].layer_norm2.weight)
+
+        assert (
+            nanotron_model.encoder[i].pp_block.layer_norm2.bias.shape == hf_model.encoder.layers[i].layer_norm2.bias.shape
+        )
+
+        with torch.no_grad():
+            nanotron_model.encoder[i].pp_block.layer_norm2.bias.copy_(hf_model.encoder.layers[i].layer_norm2.bias)
 
         # MLP
         ## FC1
@@ -343,11 +357,11 @@ def copy_weights_from_hf_to_nanotron_connector(
     log_rank("Copying weights from Idefic3 Connector to Nanotron model...", logger=logger, level=logging.INFO, rank=0)
 
     assert (
-        nanotron_model.connector.pp_block.modality_projector.proj.weight.shape == hf_model.connector.modality_projection.proj.weight.shape
+        nanotron_model.connector.modality_projector.pp_block.proj.weight.shape == hf_model.connector.modality_projection.proj.weight.shape
     )
 
     with torch.no_grad():
-        nanotron_model.connector.pp_block.modality_projector.proj.weight.copy_(hf_model.connector.modality_projection.proj.weight)
+        nanotron_model.connector.modality_projector.pp_block.proj.weight.copy_(hf_model.connector.modality_projection.proj.weight)
 
     log_rank("Copied Connector", logger=logger, level=logging.INFO, rank=0)
 

tools/idefics3/generate_hf_predictions.py
@@ -14,6 +14,7 @@ import torch
 
 from transformers import AutoProcessor, AutoModelForVision2Seq
 from transformers.image_utils import load_image
+from transformers.modeling_flash_attention_utils import _flash_attention_forward
 
 DEVICE = torch.device("cuda")
 TORCH_DTYPE = torch.bfloat16
@@ -82,6 +83,234 @@ def forward_embedding(
         embeddings = embeddings + self.position_embedding(position_ids)
         return embeddings
 
+def forward_attn(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.LongTensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value = None,
+        output_attentions: bool = False,
+        use_cache: bool = False,
+        layer_idx: int = 0,
+        **kwargs,
+    ):
+        output_attentions = False
+
+        bsz, q_len, _ = hidden_states.size()
+
+        query_states = self.q_proj(hidden_states)
+        key_states = self.k_proj(hidden_states)
+        value_states = self.v_proj(hidden_states)
+
+        combined = torch.cat([key_states, value_states, query_states], dim=-1)
+
+        torch.save(combined, f"img_emb_hf_{layer_idx}_qkv.pt")
+
+        # Flash attention requires the input to have the shape
+        # batch_size x seq_length x head_dim x hidden_dim
+        # therefore we just need to keep the original shape
+        query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim)
+        key_states = key_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        value_states = value_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+
+        kv_seq_len = key_states.shape[-2]
+        if past_key_value is not None:
+            kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
+
+        # TODO: These transpose are quite inefficient but Flash Attention requires the layout [batch_size, sequence_length, num_heads, head_dim]. We would need to refactor the KV cache
+        # to be able to avoid many of these transpose/reshape/view.
+        key_states = key_states.transpose(1, 2)
+        value_states = value_states.transpose(1, 2)
+
+        dropout_rate = self.dropout if self.training else 0.0
+
+        # In PEFT, usually we cast the layer norms in float32 for training stability reasons
+        # therefore the input hidden states gets silently casted in float32. Hence, we need
+        # cast them back in the correct dtype just to be sure everything works as expected.
+        # This might slowdown training & inference so it is recommended to not cast the LayerNorms
+        # in fp32. (Idefics2VisionRMSNorm handles it correctly)
+
+        input_dtype = query_states.dtype
+        if input_dtype == torch.float32:
+            if torch.is_autocast_enabled():
+                target_dtype = torch.get_autocast_gpu_dtype()
+            # Handle the case where the model is quantized
+            elif hasattr(self.config, "_pre_quantization_dtype"):
+                target_dtype = self.config._pre_quantization_dtype
+            else:
+                target_dtype = self.q_proj.weight.dtype
+
+            # logger.warning_once(
+            #     f"The input hidden states seems to be silently casted in float32, this might be related to"
+            #     f" the fact you have upcasted embedding or layer norm layers in float32. We will cast back the input in"
+            #     f" {target_dtype}."
+            # )
+
+            query_states = query_states.to(target_dtype)
+            key_states = key_states.to(target_dtype)
+            value_states = value_states.to(target_dtype)
+
+        torch.save(query_states, f"img_emb_hf_{layer_idx}_query_states.pt")
+        torch.save(key_states, f"img_emb_hf_{layer_idx}_key_states.pt")
+        torch.save(value_states, f"img_emb_hf_{layer_idx}_value_states.pt")
+        torch.save(attention_mask, f"img_emb_hf_{layer_idx}_attention_mask.pt")
+        torch.save((q_len, dropout_rate, self.is_causal, self._flash_attn_uses_top_left_mask), f"img_emb_hf_{layer_idx}_args.pt")
+
+        attn_output = _flash_attention_forward(
+            query_states,
+            key_states,
+            value_states,
+            attention_mask,
+            q_len,
+            dropout=dropout_rate,
+            is_causal=self.is_causal,
+            use_top_left_mask=self._flash_attn_uses_top_left_mask,
+        )
+
+        attn_output = attn_output.reshape(bsz, q_len, self.embed_dim).contiguous()
+        
+        
+        torch.save(attn_output, f"img_emb_hf_{layer_idx}_attn_output.pt")
+        
+        attn_output = self.out_proj(attn_output)
+
+        if not output_attentions:
+            attn_weights = None
+
+        return attn_output, attn_weights
+
+
+def forward_encoder_layer(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: torch.Tensor,
+        layer_idx: int,
+        output_attentions: Optional[bool] = False,
+    ):
+        """
+        Args:
+            hidden_states (`torch.FloatTensor`):
+                Input to the layer of shape `(batch, seq_len, embed_dim)`.
+            attention_mask (`torch.FloatTensor`):
+                Attention mask of shape `(batch, 1, q_len, k_v_seq_len)` where padding elements are indicated by very large negative values.
+            output_attentions (`bool`, *optional*, defaults to `False`):
+                Whether or not to return the attentions tensors of all attention layers. See `attentions` under
+                returned tensors for more detail.
+        """
+        residual = hidden_states
+
+        torch.save(hidden_states, f"img_emb_hf_{layer_idx}_before_ln1.pt")
+
+        hidden_states = self.layer_norm1(hidden_states)
+
+        torch.save(hidden_states, f"img_emb_hf_{layer_idx}_after_ln1.pt")
+
+        hidden_states, attn_weights = forward_attn(
+            self.self_attn,
+            hidden_states=hidden_states,
+            attention_mask=attention_mask,
+            output_attentions=output_attentions,
+            layer_idx=layer_idx,
+        )
+
+        torch.save(hidden_states, f"img_emb_hf_{layer_idx}_after_attn.pt")
+
+        hidden_states = residual + hidden_states
+
+        torch.save(hidden_states, f"img_emb_hf_{layer_idx}_before_ln2.pt")
+
+        residual = hidden_states
+        hidden_states = self.layer_norm2(hidden_states)
+
+        torch.save(hidden_states, f"img_emb_hf_{layer_idx}_after_ln2.pt")
+
+        hidden_states = self.mlp(hidden_states)
+
+        torch.save(hidden_states, f"img_emb_hf_{layer_idx}_after_mlp.pt")
+
+        hidden_states = residual + hidden_states
+
+        outputs = (hidden_states,)
+
+        if output_attentions:
+            outputs += (attn_weights,)
+
+        return outputs
+
+# Ignore copy
+def forward_encoder(
+    self,
+    inputs_embeds,
+    attention_mask: Optional[torch.Tensor] = None,
+    output_attentions: Optional[bool] = None,
+    output_hidden_states: Optional[bool] = None,
+    return_dict: Optional[bool] = None,
+):
+    r"""
+    Args:
+        inputs_embeds (`torch.FloatTensor` of shape `(batch_size, sequence_length, hidden_size)`):
+            Optionally, instead of passing `input_ids` you can choose to directly pass an embedded representation.
+            This is useful if you want more control over how to convert `input_ids` indices into associated vectors
+            than the model's internal embedding lookup matrix.
+        attention_mask (`torch.Tensor` of shape `(batch_size, sequence_length)`, *optional*):
+            Mask to avoid performing attention on padding token indices. Mask values selected in `[0, 1]`:
+
+            - 1 for tokens that are **not masked**,
+            - 0 for tokens that are **masked**.
+
+            [What are attention masks?](../glossary#attention-mask)
+        output_attentions (`bool`, *optional*):
+            Whether or not to return the attentions tensors of all attention layers. See `attentions` under
+            returned tensors for more detail.
+        output_hidden_states (`bool`, *optional*):
+            Whether or not to return the hidden states of all layers. See `hidden_states` under returned tensors
+            for more detail.
+        return_dict (`bool`, *optional*):
+            Whether or not to return a [`~utils.ModelOutput`] instead of a plain tuple.
+    """
+    output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+    output_hidden_states = (
+        output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+    )
+    return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+    encoder_states = () if output_hidden_states else None
+    all_attentions = () if output_attentions else None
+
+    hidden_states = inputs_embeds
+    for i, encoder_layer in enumerate(self.layers):
+        if output_hidden_states:
+            encoder_states = encoder_states + (hidden_states,)
+        if self.gradient_checkpointing and self.training:
+            layer_outputs = self._gradient_checkpointing_func(
+                encoder_layer.__call__,
+                hidden_states,
+                attention_mask,
+                output_attentions,
+            )
+        else:
+            layer_outputs = forward_encoder_layer(
+                encoder_layer,
+                hidden_states,
+                attention_mask,
+                layer_idx=i,
+                output_attentions=output_attentions,
+            )
+
+        hidden_states = layer_outputs[0]
+
+        torch.save(hidden_states, f"img_emb_hf_{i}.pt")
+
+        if output_attentions:
+            all_attentions = all_attentions + (layer_outputs[1],)
+
+    if output_hidden_states:
+        encoder_states = encoder_states + (hidden_states,)
+
+    if not return_dict:
+        return tuple(v for v in [hidden_states, encoder_states, all_attentions] if v is not None)
+    return hidden_states, encoder_states, all_attentions
+
 
 def forward_vision(
     self,
@@ -110,6 +339,8 @@ def forward_vision(
             patch_attention_mask = patch_attention_mask.to(dtype=torch.bool, device=pixel_values.device)
 
         hidden_states = forward_embedding(self.embeddings, pixel_values=pixel_values, patch_attention_mask=patch_attention_mask)
+        
+        torch.save(hidden_states, "img_emb_hf.pt")
 
         patch_attention_mask = patch_attention_mask.view(batch_size, -1)
         # The call to `_upad_input` in `_flash_attention_forward` is expensive
@@ -120,7 +351,8 @@ def forward_vision(
         elif not self._use_flash_attention_2:
             patch_attention_mask = _prepare_4d_attention_mask(patch_attention_mask, hidden_states.dtype)
 
-        encoder_outputs = self.encoder(
+        encoder_outputs = forward_encoder(
+            self.encoder,
             inputs_embeds=hidden_states,
             attention_mask=patch_attention_mask,
             output_attentions=output_attentions,
@@ -179,6 +411,9 @@ def forward(
         if inputs_embeds is None:
             inputs_embeds = self.text_model.get_input_embeddings()(input_ids).to(self.device)
 
+
+            torch.save(inputs_embeds, "inputs_embeds_hf.pt")
+
         # START VISUAL INPUTS INTEGRATION
         if pixel_values is not None and image_hidden_states is not None:
             raise ValueError("You cannot specify both pixel_values and image_hidden_states at the same time")
@@ -211,12 +446,17 @@ def forward(
             patches_subgrid = patches_subgrid.unfold(dimension=2, size=patch_size, step=patch_size)
             patch_attention_mask = (patches_subgrid.sum(dim=(-1, -2)) > 0).bool()
 
+            torch.save(pixel_values, "pv_hf.pt")
+
             # Get sequence from the vision encoder
             image_hidden_states = forward_vision(
                 self.vision_model,
                 pixel_values=pixel_values,
                 patch_attention_mask=patch_attention_mask,
             )
+
+
+            torch.save(image_hidden_states, "img_hid_hf.pt")
 
             # Modality projection & resampling  
             image_hidden_states = self.connector(image_hidden_states)
@@ -248,7 +488,7 @@ def forward(
         if not return_dict:
             return tuple(v for v in [*outputs, image_hidden_states] if v is not None)
 
-        return None
+        return outputs
 
 
 
@@ -269,17 +509,26 @@ def main(args):
     text = processor.apply_chat_template(messages, add_generation_prompt=True)
     inputs = processor(images=images, text=text, return_tensors="pt").to(DEVICE)
 
-
+    print(
+        model.model.config
+    )
 
     with torch.no_grad():
         # output = model(**inputs)
 
-        forward(
+        output = forward(
             model.model,
             use_cache=False,
             **inputs
         )
 
+    print(output)
+    print(output.last_hidden_state.shape)
+
+    torch.save(
+        output,
+        "hf_output.pt",
+    )
 
 
 if __name__ == "__main__":